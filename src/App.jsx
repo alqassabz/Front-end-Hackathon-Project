063cf38a25a99db727a2d4a656a83371e6c37e0e
zalqassabz <zara.qassab@gmail.com>
2024-10-15 15:49:30 +0300

    added modes

--- src/App.jsx
+++ src/App.jsx
@@ -4,43 +4,53 @@ import 'slick-carousel/slick/slick-theme.css'
 import Nav from './components/Nav'
 import { Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
+import ThemeToggle from './components/ThemeToggle'
+import ThemeContext from './context/ThemeContext'
+import { useContext } from 'react'
 
 const App = () => {
+  const { theme } = useContext(ThemeContext)
   return (
-    <div className="App">
-      <Nav />
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/rides" element={
-            <div className='boxes'>
-      {
-        // Do something here inside the map
-        <>
-        <div className='box'>
-        <h1>Hello</h1>
-        </div>
-        <div className='box'>
-        <h1>Hello</h1>
-        </div>
-        <div className='box'>
-        <h1>Hello</h1>
-        </div>
-        <div className='box'>
-        <h1>Hello</h1>
-        </div>
-        <div className='box'>
-        <h1>Hello</h1>
-        </div>
-        <div className='box'>
-        <h1>Hello</h1>
-        </div>
-        </>
-      }
+    <div className="App ">
+      <div className={theme}>
+        <Nav />
+
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/rides"
+              element={
+                <div className="boxes">
+                  {
+                    // Do something here inside the map
+                    <>
+                      <div className="box">
+                        <h1>Hello</h1>
+                      </div>
+                      <div className="box">
+                        <h1>Hello</h1>
+                      </div>
+                      <div className="box">
+                        <h1>Hello</h1>
+                      </div>
+                      <div className="box">
+                        <h1>Hello</h1>
+                      </div>
+                      <div className="box">
+                        <h1>Hello</h1>
+                      </div>
+                      <div className="box">
+                        <h1>Hello</h1>
+                      </div>
+                    </>
+                  }
+                </div>
+              }
+            />
+          </Routes>
+        </main>
       </div>
-    } />
-        </Routes>
-      </main>
     </div>
   )
 }
---
+++ src/components/ThemeToggle.jsx
@@ -0,0 +1,23 @@
+import { FiSun, FiMoon } from 'react-icons/fi'
+import ThemeContext from '../context/ThemeContext'
+import { useContext } from 'react'
+
+
+const ThemeToggle = () => {
+ 
+  const { toggleTheme } = useContext(ThemeContext)
+  return (
+    <div className="theme-slider-toggle" onClick={toggleTheme}>
+      <div className="toggle">
+        <div className="slider" />
+        <div className="icon-container">
+          <FiSun className="sun-icon" />
+          <div className="spacer"></div>
+          <FiMoon className="moon-icon" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ThemeToggle
