a4ea7d1d01cd1750c3d1f0d0bb78a7986e26ab81
MuathAlsabbah <muath.alsabbah@gmail.com>
2024-10-15 15:54:16 +0300

    k

--- src/components/Rides.jsx
+++ src/components/Rides.jsx
@@ -1,19 +1,22 @@
-
 const Rides = ({ rides }) => {
   return (
     <div className="rides-container">
       <h1>Rollercoasters</h1>
       <ul>
-        {rides? (rides.map((ride) => (
-          <li className="rides" key={ride.id} id={ride.id}>
-            <div className="ride-info">
-              <h3>RollerCoaster Name: {ride.title}</h3>
-              <h5>Height Limit: {ride.heightLimit}</h5>
-              <h5>Capacity: {ride.capacityLimit}</h5>
-              <h5>Death Risk: {ride.deathRisk}</h5>
+        {rides ? (
+          rides.map((ride) => (
+            <div className="rides" key={ride.id} id={ride.id}>
+              <div className="ride-info">
+                <h3>{ride.title}</h3>
+                <h5>Height Limit: {ride.heightLimit} meter</h5>
+                <h5>Capacity: {ride.capacityLimit} </h5>
+                <h5>Death Risk: {ride.deathRisk}</h5>
+              </div>
             </div>
-          </li>))) : <h1>hi</h1>}
-        
+          ))
+        ) : (
+          <h1>hi</h1>
+        )}
       </ul>
     </div>
   )
--- src/rides.js
+++ src/rides.js
@@ -1,73 +1,73 @@
 const rides = [
   {
     title: 'Cloud Chaser',
-    heightLimit: '160 cm',
+    heightLimit: '160',
     capacityLimit: '20',
     deathRisk: 'High'
   },
   {
     title: 'Devils Tower',
-    heightLimit: '170 cm',
+    heightLimit: '170',
     capacityLimit: '10',
     deathRisk: 'Medium'
   },
   {
     title: 'Wave Runner',
-    heightLimit: '140 cm',
+    heightLimit: '140',
     capacityLimit: '24',
     deathRisk: 'Low'
   },
   {
     title: 'Sandstorm',
-    heightLimit: '180 cm',
+    heightLimit: '180',
     capacityLimit: '32',
     deathRisk: 'Unavoidable'
   },
   {
     title: 'Soul Reaper',
-    heightLimit: '180 cm',
+    heightLimit: '180',
     capacityLimit: '45',
     deathRisk: 'Imminent'
   },
   {
     title: 'Concussionor',
-    heightLimit: '175 cm',
+    heightLimit: '175',
     capacityLimit: '10',
     deathRisk: 'High'
   },
   {
     title: 'Ghost Train',
-    heightLimit: '165 cm',
+    heightLimit: '165',
     capacityLimit: '22',
     deathRisk: 'Medium'
   },
   {
     title: 'While Looper',
-    heightLimit: '200 cm',
+    heightLimit: '200  ',
     capacityLimit: '30',
     deathRisk: 'Inevitable'
   },
   {
     title: 'Sky Rider',
-    heightLimit: '165 cm',
+    heightLimit: '165  ',
     capacityLimit: '26',
     deathRisk: 'Medium'
   },
   {
     title: 'Supernova ',
-    heightLimit: '140 cm',
+    heightLimit: '140  ',
     capacityLimit: '10',
     deathRisk: 'Low'
   },
   {
     title: 'Blood Bomber',
-    heightLimit: '195 cm',
+    heightLimit: '195  ',
     capacityLimit: '60',
     deathRisk: 'Inevitable'
   },
   {
     title: 'Uncharted ',
-    heightLimit: '170 cm',
+    heightLimit: '170  ',
     capacityLimit: '20',
     deathRisk: 'High'
   }
