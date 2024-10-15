15cc607503ec1646474115c626db719dbc23feb2
zalqassabz <zara.qassab@gmail.com>
2024-10-15 14:18:22 +0300

    added borders

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,44 @@
 import './App.css'
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 import Nav from './components/Nav'
 import { Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
 
-
 const App = () => {
-
   return (
-    <div className='App'>
+    <div className="App">
       <Nav />
       <main>
         <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/rides" element={<h1>Hello There</h1>} />
-        
+          <Route path="/" element={<Home />} />
+          <Route path="/rides" element={
+            <div className='boxes'>
+      {
+        // Do something here inside the map
+        <>
+        <div className='box'>
+        <h1>Hello</h1>
+        </div>
+        <div className='box'>
+        <h1>Hello</h1>
+        </div>
+        <div className='box'>
+        <h1>Hello</h1>
+        </div>
+        <div className='box'>
+        <h1>Hello</h1>
+        </div>
+        <div className='box'>
+        <h1>Hello</h1>
+        </div>
+        <div className='box'>
+        <h1>Hello</h1>
+        </div>
+        </>
+      }
+      </div>
+    } />
         </Routes>
       </main>
     </div>
