7c09d04167afeecac87dd9b512ddf87161ff1540
MuathAlsabbah <muath.alsabbah@gmail.com>
2024-10-15 23:08:10 +0300

    crazy

--- src/components/Rides.jsx
+++ src/components/Rides.jsx
@@ -1,21 +1,22 @@
 const Rides = ({ rides }) => {
   return (
     <div className="rides-container">
-      <h1>Rollercoasters</h1>
-      <ul>
+      <h1>Theme Park Rides</h1>
+      <ul className="rides-list">
         {rides ? (
           rides.map((ride) => (
-            <div className="rides" key={ride.id} id={ride.id}>
-              <div className="ride-info">
+            <li className="ride-info" key={ride.id} id={ride.id}>
+              <div className="ride-image" style={{backgroundImage: `url(placeholder-image-url-for-${ride.title})`}}></div>
+              <div className="ride-details">
                 <h3>{ride.title}</h3>
-                <h5>Height Limit: {ride.heightLimit} meter</h5>
-                <h5>Capacity: {ride.capacityLimit} </h5>
+                <h5>Height Limit: {ride.heightLimit} meters</h5>
+                <h5>Capacity: {ride.capacityLimit} riders</h5>
                 <h5>Death Risk: {ride.deathRisk}</h5>
               </div>
-            </div>
+            </li>
           ))
         ) : (
-          <h1>hi</h1>
+          <li>No rides available</li>
         )}
       </ul>
     </div>
